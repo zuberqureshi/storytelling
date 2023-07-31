8612c962314ada2fee7fd266e3c06f6b92003be0
Danish Qureshi <85394659+Danishqureshi8817@users.noreply.github.com>
2023-07-31 20:22:18 +0530

    31 - july evening

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View,StatusBar } from 'react-native'
 import React from 'react'
 import Navigation from './src/navigation/index'
+import {TranslatorProvider} from 'react-native-translator'
 
 const App = () => {
   return (
     <>
+   <TranslatorProvider>
+   <Navigation/>
+
+   </TranslatorProvider>
    
-    <Navigation/>
  
     </>
   )
